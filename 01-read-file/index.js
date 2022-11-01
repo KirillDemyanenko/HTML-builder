27b233eb0e1b8e1ef2e970d07621fe9a5faf93db
KirillDemyanenko <demyanenkokg@gmail.com>
2022-11-01 18:27:15 +0300

    refactor: refactor with prettier

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,13 @@
-const fs = require("fs"), path = require('path');
-let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
-stream.on('readable', () => {
+const fs = require("fs"),
+  path = require("path");
+let stream = new fs.ReadStream(path.join(__dirname, "text.txt"));
+stream.on("readable", () => {
   let data;
-  while (data = stream.read()) {
+  while ((data = stream.read())) {
     console.log(data.toString());
   }
 });
-stream.on('end', () => {console.log('end_of_file')})
-stream.on('error', (error) => console.log(error))
+stream.on("end", () => {
+  console.log("end_of_file");
+});
+stream.on("error", (error) => console.log(error));
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,25 @@
-let fs = require('fs');
-const path = require('path');
-fs.open(path.join(__dirname, 'file.txt'), 'w', (error) => {
-    console.log('Hello!')
-    if (error) {
-        throw new Error(error)
+let fs = require("fs");
+const path = require("path");
+fs.open(path.join(__dirname, "file.txt"), "w", (error) => {
+  console.log("Hello!");
+  if (error) {
+    throw new Error(error);
+  } else {
+    console.log(
+      "File created successfully!\n" +
+        'Enter text to write to file\n(Write "exit" for exit)'
+    );
+  }
+  process.stdin.on("data", (data) => {
+    if (data.toString().trim() === "exit") {
+      console.log("Bye!");
+      process.exit(0);
     } else {
-        console.log('File created successfully!\n' +
-            'Enter text to write to file\n(Write \"exit\" for exit)')
-    }
-    process.stdin.on('data', (data) => {
-        if (data.toString().trim() === 'exit') {
-            console.log('Bye!')
-            process.exit(0)
-        } else {
-            fs.appendFile(path.join(__dirname, 'file.txt'), data, (error) => {
-                if (error) {
-                    throw new Error(error)
-                }
-            })
+      fs.appendFile(path.join(__dirname, "file.txt"), data, (error) => {
+        if (error) {
+          throw new Error(error);
         }
-    })
-}
-)
+      });
+    }
+  });
+});
