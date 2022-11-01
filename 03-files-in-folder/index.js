33f404f970f542fc04555c1ed7f52e6b74210229
KirillDemyanenko <demyanenkokg@gmail.com>
2022-11-01 17:26:01 +0300

    03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+let fs = require('node:fs');
+let path = require('node:path');
+fs.readdir(path.join(__dirname, '/secret-folder'), (error, files) => {
+    if (error) {
+        throw new Error(error)
+    } else {
+        for (file of files) {
+            let fileName = file.split('.')
+            fs.stat(path.join(__dirname, '/secret-folder/' + file), (error, fileStats) => {
+                if (error) {
+                    throw new Error(error)
+                } else {
+                    if (fileStats.isFile()) {
+                        console.log(`${fileName[0]} - ${fileName[1]} - ${(fileStats['size'] / 1024).toFixed(3)}kb`)
+                    }
+                }
+            })
+        }
+    }
+})
