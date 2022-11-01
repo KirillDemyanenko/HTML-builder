b1daf20c289be00288a22165a9dc080f2871c705
KirillDemyanenko <demyanenkokg@gmail.com>
2022-11-01 18:28:21 +0300

    feat: solved 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
-
+let fs = require("node:fs");
+let path = require("node:path");
+fs.mkdir(path.join(__dirname, "/files-copy"), { recursive: true }, (error) => {
+  if (error) {
+    throw error;
+  } else {
+    fs.readdir(path.join(__dirname, "/files"), (error, files) => {
+      if (error) {
+        throw error;
+      } else {
+        for (file of files) {
+          fs.copyFile(
+            path.join(__dirname, "/files/" + file),
+            path.join(__dirname, "/files-copy/" + file),
+            (error) => {
+              if (error) {
+                throw error;
+              }
+            }
+          );
+        }
+      }
+    });
+  }
+});
