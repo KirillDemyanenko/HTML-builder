c9351308c9a977d6319df347b88db31fffbd381f
KirillDemyanenko <demyanenkokg@gmail.com>
2022-11-03 09:13:49 +0300

    refactor: change let on const

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,25 +1,19 @@
-let fs = require("node:fs");
-let path = require("node:path");
+const fs = require("node:fs");
+const path = require("node:path");
 fs.mkdir(path.join(__dirname, "/files-copy"), { recursive: true }, (error) => {
-  if (error) {
-    throw error;
-  } else {
-    fs.readdir(path.join(__dirname, "/files"), (error, files) => {
-      if (error) {
-        throw error;
-      } else {
-        for (file of files) {
-          fs.copyFile(
-            path.join(__dirname, "/files/" + file),
-            path.join(__dirname, "/files-copy/" + file),
-            (error) => {
-              if (error) {
-                throw error;
-              }
-            }
-          );
+  if (error) throw error;
+  fs.readdir(path.join(__dirname, "/files"), (error, files) => {
+    if (error) throw error;
+    for (let file of files) {
+      fs.copyFile(
+        path.join(__dirname, "/files/" + file),
+        path.join(__dirname, "/files-copy/" + file),
+        (error) => {
+          if (error) {
+            throw error;
+          }
         }
-      }
-    });
-  }
+      );
+    }
+  });
 });
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,14 +1,14 @@
-let fs = require("node:fs");
-let path = require("node:path");
+const fs = require("node:fs");
+const path = require("node:path");
 fs.open(path.join(__dirname, "/project-dist/bundle.css"), "w", (err) => {
   if (err) throw err;
 });
 fs.readdir(path.join(__dirname, "/styles/"), (err, files) => {
   for (let file of files) {
-    let splitedFileName = file.split(".");
-    if (splitedFileName[1] === "css") {
+    let separatedFileName = file.split(".");
+    if (separatedFileName[1] === "css") {
       fs.readFile(
-        path.join(__dirname, "/styles/" + splitedFileName.join(".")),
+        path.join(__dirname, "/styles/" + separatedFileName.join(".")),
         (err, data) => {
           if (err) throw err;
           fs.appendFile(
