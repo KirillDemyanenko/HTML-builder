388eba8b3a9949c594e57d3176398b5a345efca8
KirillDemyanenko <demyanenkokg@gmail.com>
2023-05-05 08:48:21 +0300

    refactor: simplify 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,3 @@
 const fs = require("node:fs");
-const path = require("node:path");
-const stream = new fs.ReadStream(path.join(__dirname, "text.txt"));
-stream.on("readable", () => {
-  while ((data = stream.read())) {
-    console.log(data.toString());
-  }
-});
-stream.on("end", () => {
-  console.log("end_of_file");
-});
-stream.on("error", (error) => console.error(error));
+const stream = new fs.ReadStream(__dirname.concat("/text.txt"));
+stream.pipe(process.stdout);
