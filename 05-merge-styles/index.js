883089d0699ebeddcdb230bd631c92d2aaa32022
KirillDemyanenko <demyanenkokg@gmail.com>
2023-05-05 13:19:41 +0300

    refactor: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,14 @@
 const fs = require("node:fs");
 const path = require("node:path");
-fs.open(path.join(__dirname, "/project-dist/bundle.css"), "w", (err) => {
-  if (err) throw err;
-});
-fs.readdir(path.join(__dirname, "/styles/"), (err, files) => {
-  for (let file of files) {
-    let separatedFileName = file.split(".");
-    if (separatedFileName[1] === "css") {
-      fs.readFile(
-        path.join(__dirname, "/styles/" + separatedFileName.join(".")),
-        (err, data) => {
-          if (err) throw err;
-          fs.appendFile(
-            path.join(__dirname, "/project-dist/bundle.css"),
-            data,
-            (err) => {
-              if (err) throw err;
-            }
-          );
-        }
-      );
-    }
-  }
+const writeStream = fs.createWriteStream(
+  path.join(__dirname, "/project-dist/bundle.css")
+);
+fs.readdir(path.join(__dirname, "/styles"), (error, files) => {
+  if (error) throw error;
+  files.forEach((file) => {
+    !path.extname(file).includes(".css") ||
+      fs
+        .createReadStream(path.join(__dirname, "/styles/", file))
+        .pipe(writeStream);
+  });
 });
