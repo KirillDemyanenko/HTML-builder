a09c69bed4950eba6299f1e7fbb5ab46d8b307c3
KirillDemyanenko <demyanenkokg@gmail.com>
2023-05-06 13:00:40 +0300

    refactor: 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,20 @@
 const fs = require("node:fs");
 const path = require("node:path");
-//Creates a copy of files and folders from assets
+const writeStream = fs.createWriteStream(
+  path.join(__dirname, "/project-dist/style.css")
+);
+fs.writeFile(path.join(__dirname, "/project-dist/index.html"), "", (err) => {
+  if (err) throw err;
+});
+fs.readdir(path.join(__dirname, "/styles"), (error, files) => {
+  if (error) throw error;
+  files.forEach((file) => {
+    !path.extname(file).includes(".css") ||
+      fs
+        .createReadStream(path.join(__dirname, "/styles/", file))
+        .pipe(writeStream);
+  });
+});
 fs.mkdir(
   path.join(__dirname, "project-dist/assets"),
   { recursive: true },
@@ -41,90 +55,35 @@ fs.mkdir(
     );
   }
 );
-fs.open(path.join(__dirname, "/project-dist/style.css"), "w", (err) => {
-  fs.readdir(path.join(__dirname, "/styles"), (err, files) => {
-    if (err) throw err;
-    for (let file of files) {
-      if (file.split(".")[1] === "css") {
-        fs.readFile(path.join(__dirname, "/styles/" + file), (err, data) => {
-          if (err) throw err;
+fs.readFile(path.join(__dirname, "template.html"), (err, data) => {
+  if (err) throw err;
+  const array = data.toString().split("\n");
+  array.forEach((str) => {
+    if (str.indexOf("{{") > -1) {
+      fs.readFile(
+        path.join(
+          __dirname,
+          "components",
+          str.trim().replace("{{", "").replace("}}", "").concat(".html")
+        ),
+        (err1, data1) => {
           fs.appendFile(
-            path.join(__dirname, "/project-dist/style.css"),
-            data,
+            path.join(__dirname, "/project-dist/index.html"),
+            data1.toString().concat("\n"),
             (err) => {
               if (err) throw err;
             }
           );
-        });
-      }
-    }
-  });
-});
-fs.readFile(path.join(__dirname, "template.html"), function (err, data) {
-  if (err) throw err;
-  let array = data.toString().split("\n");
-  var tempArr = [];
-  for (i in array) {
-    if (array[i].includes("{{") && array[i].includes("}}")) {
-      fs.readFile(
-        path.join(
-          __dirname,
-          "/components/" +
-            array[i].replace("{{", "").replace("}}", "").trim() +
-            ".html"
-        ),
-        (err, data1) => {
-          if (err) throw err;
-          let compArr = data1.toString().split("\n");
-          for (let c of compArr) {
-            fs.appendFile(
-              path.join(__dirname, "/project-dist/index.html"),
-              c,
-              (err) => {
-                if (err) throw err;
-              }
-            );
-          }
         }
       );
     } else {
       fs.appendFile(
         path.join(__dirname, "/project-dist/index.html"),
-        array[i],
+        str.concat("\n"),
         (err) => {
           if (err) throw err;
         }
       );
     }
-  }
-  for (let ar of tempArr) {
-    console.log(ar);
-  }
+  });
 });
-// fs.open(path.join(__dirname, "project-dist/index.html"), "r+", (err, fd) => {
-//   if (err) {
-//     fs.stat(path.join(__dirname, "/project-dist/index.html"), (err) => {
-//       if (err) {
-//         fs.stat(path.join(__dirname, "/project-dist"), (err) => {
-//           if (err) {
-//             fs.mkdir(path.join(__dirname, "/project-dist"), (err) => {});
-//           }
-//           fs.copyFile(
-//             path.join(__dirname, "template.html"),
-//             path.join(__dirname, "project-dist/index.html"),
-//             (err) => {
-//               if (err) throw err;
-//             }
-//           );
-//         });
-//       }
-//       fs.readFile(
-//         path.join(__dirname, "project-dist/index.html"),
-//         "utf-8",
-//         (err, data) => {
-//           console.log(data);
-//         }
-//       );
-//     });
-//   }
-// });
